66d347eb5c687fb2bed131b741292e8ba77798fa
ram_tanniru <ramtanniru2@gmail.com>
2024-06-27 17:31:07 +0530

    1791

---
+++ Jun-27-24.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        adj = defaultdict(list)
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        vertices = len(adj.keys())
+        for v,adjV in adj.items():
+            if len(adjV)==vertices-1:
+                return v
+        return -1 

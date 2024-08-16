422ce3e37329a5b3db28dfb148fae088493614d0
ram_tanniru <ramtanniru2@gmail.com>
2024-08-16 11:25:29 +0530

    624

--- Aug-16-24.py
+++ Aug-16-24.py
@@ -1,15 +1,11 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        res = float('-inf')
-        maxEl,minEl = [],[]
-        d = defaultdict(set)
-        for i in arrays:
-            maxEl.append(i[-1])
-            minEl.append(i[0])
-            d[i[0]].add(i[-1])
-            d[i[-1]].add(i[0])
-        maxEl.sort()
-        minEl.sort()
-        if not(minEl[0] in d[maxEl[-1]] or maxEl[-1] in d[minEl[0]]):
-            return maxEl[-1]-minEl[0]
-        return max(maxEl[-1]-minEl[1],maxEl[-2]-minEl[0]) 
+        small = arrays[0][0]
+        big = arrays[0][-1]
+        res = 0
+        for i in range(1,len(arrays)):
+            arr = arrays[i]
+            res = max(res,abs(arr[-1]-small),abs(big-arr[0]))
+            big = max(big,arr[-1])
+            small = min(small,arr[0])
+        return res 
